028ba7b19d5c0f85cdd5c5bf2d0e22eb2aefbbdb
DeshanHiripitiya <nipunadeshanhp@gmail.com>
2024-10-24 19:21:36 +0530

    backend authentication

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { register_user, login_user } = require('../../controller/register');
+const { register_user, login_user, get_all_users, edit_user,delete_user } = require('../../controller/user');
 const router = express.Router();
 
 router.post('/register', register_user);
 router.post('/login', login_user);
+router.get('/users',get_all_users);
+router.put('/user/:id',edit_user);
+router.delete('/user/:id', delete_user);
 
 module.exports = router;
